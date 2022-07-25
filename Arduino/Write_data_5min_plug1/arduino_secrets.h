ca1cd4a69670f31b371a6fabac7223dafcc51e33
whoispim <pimbot@bk.ru>
2022-07-25 12:29:20 +0200

    reorganized files

--- Arduino/Write_data_5min_plug1/arduino_secrets.h
+++
@@ -1,3 +0,0 @@
-// Copy these keys from The Things Network console and paste them here:
-const char *appEui = "A8610A34312F890B";
-const char *appKey = "00C636492B974CA74A7E18CB9CAA0D24";
---
+++ Arduino/with_lora/arduino_secrets_example.h
@@ -0,0 +1,3 @@
+// Copy these keys from The Things Network console and paste them here:
+const char *appEui = "AAAAAAAAAAAAAAAA";
+const char *appKey = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB";
